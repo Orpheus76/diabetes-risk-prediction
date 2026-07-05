6ddcad8217f1eb3e111092b70c7b0774b7c16602
Orpheus76 <lambertkylian76@gmail.com>
2026-07-05 07:00:15 +0200

    test: ajout du test de vérification de l'imputation par médiane

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,5 +1,6 @@
+import numpy as np
 import pandas as pd
-from src.preprocessing import split_data, impute_missing_values
+from src.preprocessing import split_data, impute_missing_values, COLS_TO_IMPUTE
 
 
 df = pd.read_csv("data/raw/diabetes.csv")
@@ -38,3 +39,16 @@ def test_random_state_different_gives_different_split():
     assert not X_test1.equals(X_test2)
     assert not y_train1.equals(y_train2)
     assert not y_test1.equals(y_test2)
+
+
+def test_imputation_replaces_zeros_with_median():
+    X_train, X_test, y_train, y_test = split_data(df)
+    X_train_imputed, X_test_imputed = impute_missing_values(X_train, X_test)
+
+    for c in COLS_TO_IMPUTE:
+        assert (X_train_imputed[c] == 0).sum() == 0
+        assert (X_test_imputed[c] == 0).sum() == 0
+
+        median = X_train[c].replace(0, np.nan).median()
+        assert (X_train_imputed[c] == median).sum() > 0
+        assert (X_test_imputed[c] == median).sum() > 0
